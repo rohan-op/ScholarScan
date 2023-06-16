7680e33f85a1bf7c64e39391a67f1f16b7e2543f
rohan-op <rsoh47@gmail.com>
2023-06-16 17:07:35 +0530

    Problems with chat history memory

--- app.py
+++ app.py
@@ -41,17 +41,6 @@ def get_conversation_chain(vectorstore):
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
     return conversation_chain
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question':user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i,message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
-
-
 def main():
     load_dotenv()
     st.set_page_config(page_title="Review Generator", page_icon=":books:")
--- app2.py
+++ app2.py
@@ -2,13 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
-
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain import HuggingFaceHub
+
+from langchain.prompts import PromptTemplate
+from langchain.schema import (SystemMessage, HumanMessage)
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ChatMessageHistory
+
 
 def get_summary_text(pdf_docs):
     generate_summary_llm = HuggingFaceHub(repo_id="facebook/bart-large-cnn", model_kwargs={"temperature": 0.9, "max_length":1000, "min_length":300})
@@ -24,89 +25,65 @@ def get_summary_text(pdf_docs):
     return summary
 
 
-def get_conversation_chain():
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, memory=memory)
-    return conversation_chain
-
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question':user_question+st.session_state.summary})
-    st.session_state.chat_history = response['chat_history']
-
-    for i,message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+def get_chat_history():
+    chat = ChatOpenAI(temperature=1)
+    history = ChatMessageHistory()
+    return chat, history
 
 
 def main():
+    #load api keys
     load_dotenv()
+
+    # streamlit initialize page
     st.set_page_config(page_title="Review Generator", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
-    summary = ''
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    st.header("Review Generator :books:")
+
+    # uninitialized session variables 
     if "summary" not in st.session_state:
         st.session_state.summary = None
 
-    st.header("Review Generator :books:")
-    user_question = st.text_input("Generate the Introduction, Abstract, Literature Review etc")
-    # if user_question:
-        # handle_userinput(user_question)
-
-
+    # streamlit navbar
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFS here and click on process", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
-                st.session_state.summary = get_summary_text(pdf_docs)
+                # Get summaries of all the research papers
+                #st.session_state.summary = get_summary_text(pdf_docs)
+                st.session_state.summary = '''
                 
+                '''
 
-    # if summary != '':
-    #     st.write(summary)
-    chat = ChatOpenAI(temperature=1)
-                
-    messages = [
-        SystemMessage(content="You are nice assistant")
-    ]
+    prompt = PromptTemplate(
+        input_variables= ["sections", "numwords"],
+        template = "Write the {sections} for this academic review paper. The {sections} should be {numwords} atleast words long"
+    )
+    # sections_list = ["Title","Introduction","Literature Review", "Comparison", "Conclusion", "References"]
+    # numwords_list = ["4","2000","2000","2000","1000","200"]
 
-    #   if user_question:
-    st.write(user_template.replace("{{MSG}}","Abstract"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Abstract for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
+    
+    sections_list = ["Title","Introduction","Literature Review"]
+    numwords_list = ["4","2000","2000"]
+    
+    # Initialise the chatbot with history
+    chat, history = get_chat_history()
 
-    st.write(user_template.replace("{{MSG}}","Introduction"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Introduction for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
+    # Setup Intructions for the chatbot
+    history.add_system_message("""
+    You are given summaries of multiple research papers below. Your job is to write different sections of an academic review paper. For example the Introduction, Abstract, Literature Review, Comparision, Conclusion, Acknowledgements, References etc. The user will provide the section that is to be wrriten and how long it should be. All the sections should be unique and should not be same as other sections of the paper. If you are using direct/same sentences from the summary of any research paper, mention the reference in the text. All the sections should be written in an academic manner.
 
-    st.write(user_template.replace("{{MSG}}","Literature Review"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Literature Review for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
+    SUMMARIES OF ALL THE RESEARCH PAPERS: 
+    """+st.session_state.summary)
 
-    st.write(user_template.replace("{{MSG}}","Results"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Results for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
 
-    st.write(user_template.replace("{{MSG}}","Discussion"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Discussion for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
-
-    st.write(user_template.replace("{{MSG}}","Conclusion"),unsafe_allow_html=True)
-    messages.append(HumanMessage(content="Can you write a 600 to 700 word Conclusion for an academic review paper, considering the information mentioned below about several research papers?\n\n"+st.session_state.summary))
-    response = chat(messages)
-    st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
+    # Generate Sections of Academic Review Paper:
+    for section, numword in sections_list, numwords_list:
+        history.add_user_message(prompt.format(sections=section,numwords=numword))
+        response = chat(history.messages)
+        st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
+        history.add_ai_message(response.content)
 
 if __name__ == '__main__':
     main()

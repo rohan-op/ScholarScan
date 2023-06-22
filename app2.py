55d68826180108e9ef59e1c5cb27897cb037a544
rohan-op <rsoh47@gmail.com>
2023-06-22 21:35:27 +0530

    update

--- app2.py
+++ app2.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+import os
 
 from htmlTemplates import css, bot_template, user_template
 from langchain import HuggingFaceHub
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.docstore.document import Document
+
 
 from langchain.prompts import PromptTemplate
 from langchain.schema import (SystemMessage, HumanMessage)
@@ -22,6 +30,43 @@ def get_summary_text(pdf_docs):
             text += pages.extract_text()
         summary += "\nResearch Paper " + str(i) + ':\n\n'
         summary += generate_summary_llm(text) + '\n\n'
+    return summary
+
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator = '\n',
+        chunk_size = 700,
+        chunk_overlap = 50,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+
+def get_summary(pdf_docs):
+    llm = OpenAI(temperature=1)
+    summary = ''
+    prompt_template = """Write a 400 word long summary of the following research paper, also mention the title and author of the research paper: {text}"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+    for i,doc in enumerate(pdf_docs):
+        # Get the file name and extension
+        file_name = doc.name
+        file_extension = os.path.splitext(file_name)[1].lower()
+
+        # Process only text files (e.g., .txt)
+        if file_extension == ".txt":
+            # Load the text file using TextLoader
+            loader = TextLoader(file_name)
+            documents = loader.load()
+            
+            # Split the documents using RecursiveCharacterTextSplitter
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            texts = text_splitter.split_documents(documents)
+            #chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
+            chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+            summary += "\n\nRESEARCH PAPER "+ str(i) +":\n\n"
+            summary += chain.run(texts)
     return summary
 
 
@@ -51,39 +96,39 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 # Get summaries of all the research papers
-                #st.session_state.summary = get_summary_text(pdf_docs)
-                st.session_state.summary = '''
-                
-                '''
+                st.session_state.summary = get_summary(pdf_docs)
 
     prompt = PromptTemplate(
         input_variables= ["sections", "numwords"],
         template = "Write the {sections} for this academic review paper. The {sections} should be {numwords} atleast words long"
     )
-    # sections_list = ["Title","Introduction","Literature Review", "Comparison", "Conclusion", "References"]
-    # numwords_list = ["4","2000","2000","2000","1000","200"]
+    sections_list = ["Title","Introduction","Literature Review", "Comparison", "Conclusion", "References", "Abstract"]
+    numwords_list = ["4","2000","2000","2000","1000","200","400"]
 
     
-    sections_list = ["Title","Introduction","Literature Review"]
-    numwords_list = ["4","2000","2000"]
+    # sections_list = ["Title","Introduction","Literature Review"]
+    # numwords_list = ["4","2000","2000"]
     
     # Initialise the chatbot with history
     chat, history = get_chat_history()
 
     # Setup Intructions for the chatbot
-    history.add_system_message("""
-    You are given summaries of multiple research papers below. Your job is to write different sections of an academic review paper. For example the Introduction, Abstract, Literature Review, Comparision, Conclusion, Acknowledgements, References etc. The user will provide the section that is to be wrriten and how long it should be. All the sections should be unique and should not be same as other sections of the paper. If you are using direct/same sentences from the summary of any research paper, mention the reference in the text. All the sections should be written in an academic manner.
+    messages = [
+        SystemMessage(content='''
+        You are given summaries of multiple research papers below. Your job is to write different sections of an academic review paper. For example the Introduction, Abstract, Literature Review, Comparision, Conclusion, Acknowledgements, References etc. The user will provide the section that is to be wrriten and how long it should be. All the sections should be unique and should not be same as other sections of the paper. If you are using direct/same sentences from the summary of any research paper, mention the reference in the text. All the sections should be written in an academic manner. 
 
-    SUMMARIES OF ALL THE RESEARCH PAPERS: 
-    """+st.session_state.summary)
+        NOTE: When writing an Introduction, do not mention what the summaries of the research paper or their content. Rather focus on the context and topic of the academic paper and what we plan to achieve from this review.
 
+        SUMMARIES OF ALL THE RESEARCH PAPERS: 
+        '''+str(st.session_state.summary))
+    ]
+    st.write(bot_template.replace("{{MSG}}",str(st.session_state.summary)),unsafe_allow_html=True)
 
     # Generate Sections of Academic Review Paper:
-    for section, numword in sections_list, numwords_list:
-        history.add_user_message(prompt.format(sections=section,numwords=numword))
-        response = chat(history.messages)
+    for section, numword in zip(sections_list, numwords_list):
+        messages.append(HumanMessage(content=prompt.format(sections=section,numwords=numword)))
+        response = chat(messages)
         st.write(bot_template.replace("{{MSG}}",response.content),unsafe_allow_html=True)
-        history.add_ai_message(response.content)
 
 if __name__ == '__main__':
     main()
